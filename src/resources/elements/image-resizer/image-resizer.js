b8d0777f2b98b37ea95508bf1126ece064611430
Hadrien Lanneau <hadrien@lanneau.me>
2017-04-12 11:27:44 +0200

    Display input image in resizer

--- src/resources/elements/image-resizer/image-resizer.js
+++ src/resources/elements/image-resizer/image-resizer.js
@@ -1,31 +1,84 @@
-import {observable} from 'aurelia-framework';
+import {inject, bindable} from 'aurelia-framework';
 
+function ratio(w, h) {
+  return w / h;
+}
+
+@inject(Element)
 export class ImageResizerCustomElement {
 
-  @observable file;
+  @bindable input;
+  @bindable output;
+
+  _movable = false;
+  x = 0;
+  y = 0;
+
+  imgDims = {
+    width: 0,
+    height: 0,
+    get ratio() {
+      return ratio(this.width, this.height);
+    }
+  };
+
+  constructor(element) {
+    this.element = element;
+  }
 
   attached() {
     this.supported = (window.File && window.FileReader && window.FileList && window.Blob);
+    this._listeners = {};
+    this.element.addEventListener('mousedown', this._listeners.mousedown = e => this._movable = true);
+    this.element.addEventListener('mousemove', this._listeners.mousemove = e => this._moveInput(e));
+    document.addEventListener('mouseup', this._documentListeners = e => this._movable = false);
   }
 
-  update(e) {
-    const file = e.target.files && e.target.files[0];
-    if (!file ||
-        !file.type.match('image.*')) return;
-
-    const reader = new FileReader();
-    reader.onload = fileE => {
-      this.preview = fileE.target.result;
-    };
-    reader.readAsDataURL(file);
+  detached() {
+    for (const event of Object.keys(this._listeners)) {
+      this.element.removeEventListener(event, this._listeners[event]);
+    }
+    document.removeEventListener('mouseup', this._documentListeners);
   }
 
+  loaded() {
+    if (!this.img) return;
+    const box = this.img.getBoundingClientRect();
+    this.imgDims.width = box.width;
+    this.imgDims.height = box.height;
+    this._zoom(1);
+    this.y = 0;
+    this.x = 0;
+  }
+
+  _zoom(zoom = 1) {
+    const box = this.element.getBoundingClientRect();
+    const elWidth = box.width;
+    const elHeight = box.height;
+
+    if (this.imgDims.ratio > 1) {
+      this.img.style.height = `${elHeight * zoom}px`;
+      this.img.style.width = 'auto';
+    } else {
+      this.img.style.width = `${elWidth * zoom}px`;
+      this.img.style.height = 'auto';
+    }
+  }
+
+  _moveInput(e) {
+    if (!this._movable) return;
+    e.preventDefault();
+    this.y += e.movementY;
+    this.x += e.movementX;
+  }
+
+  // TODO : resize and set ouput
   _resizeCanvas(file) {
     const ctx = this.canvas.getContext('2d');
     const img = new Image();
     img.onload = () => {
       ctx.drawImage(img, 0, 0);
     };
-    img.src = file;
+    this.output = file;
   }
 }
